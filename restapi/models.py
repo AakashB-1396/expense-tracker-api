467a0b139e132d5273bdf2cd1d082df2064c00fd
AakashB-1396 <aakashbolisetty@gmail.com>
2021-11-18 11:13:51 -0800

    Added model for expenses. Added tests for it

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    merchant = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,13 +1,17 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
+
 
 # Create your tests here.
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=20.99,
+            merchant="Funk Pop",
+            description="Wanda from WandaVision",
+            category="merchandise",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
 
-
-def two_integers_sum(a, b):
-    return a + b
-
-
-class TesSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 2), 3)
+        self.assertEqual(20.99, inserted_expense.amount)
+        self.assertEqual("Wanda from WandaVision", inserted_expense.description)
