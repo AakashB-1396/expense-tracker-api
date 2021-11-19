db6517bbe072656f5af09f1f37711278ad03115e
AakashB-1396 <aakashbolisetty@gmail.com>
2021-11-18 16:36:36 -0800

    Added expense model serializer and a negative tes without all the required fields

--- restapi/tests.py
+++ restapi/tests.py
@@ -20,7 +20,7 @@ class TestModels(TestCase):
 class TestViews(TestCase):
     def test_expense_create(self):
         payload = {
-            "amount": 25,
+            "amount": 25.0,
             "merchant": "Marvel",
             "description": "Spiderman No Way Home Tshirt",
             "category": "Marchandise",
@@ -34,7 +34,7 @@ class TestViews(TestCase):
 
         json_res = res.json()
 
-        self.assertEqual(str(payload["amount"]), json_res["amount"])
+        self.assertEqual(payload["amount"], json_res["amount"])
         self.assertEqual(payload["merchant"], json_res["merchant"])
         self.assertEqual(payload["description"], json_res["description"])
         self.assertEqual(payload["category"], json_res["category"])
@@ -51,3 +51,16 @@ class TestViews(TestCase):
 
         expenses = models.Expense.objects.all()
         self.assertEqual(len(expenses), len(json_res))
+
+    def test_expense_create_required_fields_missing(self):
+        payload = {
+            "merchant": "Marvel",
+            "description": "Spiderman No Way Home Tshirt",
+            "category": "Marchandise",
+        }
+
+        res = self.client.post(
+            reverse("restapi:expense-list-create"), payload, format="json"
+        )
+
+        self.assertEqual(400, res.status_code)
